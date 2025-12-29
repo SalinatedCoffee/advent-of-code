fc582a4060c1f6afed03484cab6a1612e5dc78e7
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2025-12-29 14:53:12 +0900

    Complete day 6 part 1

--- 2025/6/day6p1.py
+++ 2025/6/day6p1.py
@@ -1,6 +1,11 @@
-INPUT_FNAME = "test.input"
+from functools import reduce
+
+INPUT_FNAME = "day6.input"
 
 ans = 0
+add = lambda x, y: x + y
+mul = lambda x, y: x * y
+lines = []
 
 with open(INPUT_FNAME) as file:
     # load and split last line of file to retrieve columnar operators
@@ -11,7 +16,14 @@ with open(INPUT_FNAME) as file:
     # read:
     # https://stackoverflow.com/questions/46258499/how-to-read-the-last-line-of-a-file-in-python
     # https://docs.python.org/3/tutorial/inputoutput.html (f.seek())
-    break
+    # naive version first
+    for line in file:
+        lines.append(list(line.rstrip().split()))
+    m, n = len(lines), len(lines[0])
+    for i in range(n):
+        op = add if lines[-1][i] == '+' else mul
+        temp = reduce(op, [int(lines[j][i]) for j in range(m-1)])
+        ans += temp
 
 print(f"The sum of answers is {ans}.")
 
