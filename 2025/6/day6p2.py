d6381a299403c1fd2959ec84427c6c2e9d1659a3
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2025-12-30 17:14:44 +0900

    Complete day 6 part 2

--- 2025/6/day6p2.py
+++ 2025/6/day6p2.py
@@ -7,23 +7,32 @@ add = lambda x, y: x + y
 mul = lambda x, y: x * y
 lines = []
 
+def ident_col(ln: str, s: int) -> int:
+    while ln[s] == ' ':
+        s -= 1
+    return s
+
 with open(INPUT_FNAME) as file:
-    # load and split last line of file to retrieve columnar operators
-    # read line by line (doesn't even have to be sequential reads)
-    # reduce each term using operators retrieved during the first step
-    # dunno how python handles file reads but in theory this only requires
-    # at most 3 lines to be held in memory
-    # read:
-    # https://stackoverflow.com/questions/46258499/how-to-read-the-last-line-of-a-file-in-python
-    # https://docs.python.org/3/tutorial/inputoutput.html (f.seek())
-    # naive version first
+    # can't use split since we need to conserve whitespaces
+    # use last row to determine 'width' of each column
+    # can probably use reduce() to generate each columnar digit
     for line in file:
-        lines.append(list(line.rstrip().split()))
+        lines.append(line.rstrip('\n'))
     m, n = len(lines), len(lines[0])
-    for i in range(n):
-        op = add if lines[-1][i] == '+' else mul
-        temp = reduce(op, [int(lines[j][i]) for j in range(m-1)])
-        ans += temp
+    i = n-1
+    while i >= 0:
+        s = ident_col(lines[-1], i)
+        # there's gotta be a better way of doing this
+        op = add if lines[-1][s] == '+' else mul
+        res = 0 if lines[-1][s] == '+' else 1
+        for j in range(i, s-1, -1):
+            num_str = reduce(lambda x, y: x + y, [lines[k][j] for k in range(m-1)]).strip()
+            #print(f'{(num_str)}')
+            res = op(res, int(num_str))
+        ans += res
+        #print(res)
+        i = s - 2
+
 
 print(f"The sum of answers is {ans}.")
 
